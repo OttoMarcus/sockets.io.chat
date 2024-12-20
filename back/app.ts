f2e3691c976e45f2e991765018c38b73a125ae66
Vadym Benedyk <o.oo.o@zoho.com>
2024-12-20 17:38:15 +0200

    server.ts fit not starts

--- back/app.ts
+++ back/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { join } from 'node:path';
+import { io } from "./server";
 
 
 const app = express();
@@ -12,5 +13,21 @@ app.use(express.static(join(__dirname, 'public')));
 app.get('/', (req: Request, res: Response) => {
     res.sendFile(join(__dirname, 'public', 'index.html'));
 });
+
+io.of('/admin').on("connection", (socket) => {
+    console.log("Admin connected");
+    socket.on("Admin:message", (data: string) => {
+        console.log(`Message from admin: ${data}`);
+    })
+})
+
+io.of('/user').on('connection', (socket) => {
+    console.log("User connected");
+    socket.on("User:message", (data: string) => {
+        console.log(`Message from user: ${data}`);
+    })
+})
+
+
 
 export default app;
--- back/server.ts
+++ back/server.ts
@@ -9,19 +9,19 @@ const PORT: number = parseInt(process.env.PORT || "3000", 10);
 const server = http.createServer(app);
 
 // Підключаємо Socket.IO до HTTP-сервера
-const io = new Server(server, {
+export const io = new Server(server, {
     cors: {
         origin: "*", // Налаштуйте для безпеки, якщо необхідно
     },
 });
 
-io.on("connection", (socket) => {
-    console.log("A user connected");
-
-    socket.on("disconnect", () => {
-        console.log("A user disconnected");
-    });
-});
+// io.on("connection", (socket) => {
+//     console.log("A user connected");
+//
+//     socket.on("disconnect", () => {
+//         console.log("A user disconnected");
+//     });
+// });
 
 // Запускаємо сервер
 server.listen(PORT, (err?: Error) => {
