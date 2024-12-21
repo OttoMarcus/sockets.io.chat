52ab90c1e6b242bdb213d96bba86ac1a7d43eb2a
Vadym Benedyk <o.oo.o@zoho.com>
2024-12-21 15:43:09 +0200

    socket connection with html established

--- back/src/socket/user.ts
+++ back/src/socket/user.ts
@@ -3,13 +3,28 @@ import { Namespace } from "socket.io";
 const userSocketHandler = (namespace: Namespace) => {
     namespace.on("connection", (socket) => {
         //"connection" - used on server side, "connect" - used on client side
+
+        if (socket.connected) {
         console.log("Connected to user namespace", socket.id);
 
-        // socket.emit("welcome", "Hello, Guest!");
 
         socket.on("User:message", (data: string) => {
-            console.log("Message from admin:", data);
+            try {
+                console.log("Message from user:", data);
+                // Логіка для обробки даних, якщо потрібна
+                const response = `Hello, Guest! Received your message: ${data}`;
+
+                // Відправлення відповіді клієнту
+                socket.emit("welcome", response);
+            } catch (error) {
+                console.error("Error processing message:", error);
+            }
         });
+
+        } else {
+            alert("Connection lost. Please try again.");
+        }
+
 
         // socket.on("disconnect", () => {
         //     console.log("User disconnected", socket.id);
